7babb5d75226f39dc03ec5f7e910223b32412ca4
Ashok kumar <112111053+Ashokali@users.noreply.github.com>
2024-04-17 16:09:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <h1 className="main-head">Hello, Click the Button !</h1>
+        <button className="hello-btn">Click Me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
